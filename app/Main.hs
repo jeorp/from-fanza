98347c76bc392d1480c58396e240a8104a0e10bf
jeorp <jeorpqxx@gmail.com>
2022-01-18 17:10:42 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,10 @@ import Test.WebDriver
 import Control.Monad.IO.Class
 import Test.WebDriver.Session
 import qualified Data.ByteString.Lazy.Char8 as B
+import Network.HTTP.Fanza
 
-main :: IO ()
-main =
-  runSession defaultConfig $ do
-    --getSession >>= liftIO . print
-    openPage "http://google.co.jp/"
-    --screenshotWriteFile "google.png"
+main :: IO () -- sample
+main = runNewWD $ entryFanza >> extractSampleUrl "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=pppd00927/"
 
 screenshotWriteFile::  FilePath -> WD ()
 screenshotWriteFile name = do
