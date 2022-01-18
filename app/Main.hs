9b0cddf68d2f71f107d5e9a82b821e758f5fcf3d
jeorp <jeorpqxx@gmail.com>
2022-01-18 18:13:13 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,10 @@ import Test.WebDriver
 import Control.Monad.IO.Class
 import Test.WebDriver.Session
 import qualified Data.ByteString.Lazy.Char8 as B
-import Network.HTTP.Fanza
+import Network.HTTP.Fanza.Sample
 
 main :: IO () -- sample
-main = runNewWD $ entryFanza >> extractSampleUrl "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=pppd00927/"
+main = runNewWD $ entryFanza >> extractSampleMovieUrl "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=pppd00927/"
 
 screenshotWriteFile::  FilePath -> WD ()
 screenshotWriteFile name = do
