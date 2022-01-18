71ad67fd4aeb9f5525cd25e29477c50f50bf4ef5
jeorp <jeorpqxx@gmail.com>
2022-01-18 13:21:41 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Test.WebDriver
 import Control.Monad.IO.Class
+import Test.WebDriver.Session
 import qualified Data.ByteString.Lazy.Char8 as B
 
 main :: IO ()
 main =
   runSession defaultConfig $ do
+    --getSession >>= liftIO . print
     openPage "http://google.co.jp/"
-    screenshotWriteFile "google.png"
+    --screenshotWriteFile "google.png"
 
 screenshotWriteFile::  FilePath -> WD ()
 screenshotWriteFile name = do
